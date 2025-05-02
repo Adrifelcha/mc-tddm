cbe4d6f6ceedfb94050371a3c1a0bed131f2748b
Adriana Chavez <adrifelcha@gmail.com>
2025-05-02 16:05:25 -0700

    Major fix: drift angle and length were passed in the wrong order to polarToRect() functiongit add complete_parList.R

--- src/set_up/complete_parList.R
+++ src/set_up/complete_parList.R
@@ -4,8 +4,9 @@ complete_par_list <- function(par){
     par$angle <- rectToPolar(par$mu1, par$mu2)$dAngle
     }
     if(is.null(par$mu1)){
-        par$mu1 <- polarToRect(par$drift, par$angle)$x
-        par$mu2 <- polarToRect(par$drift, par$angle)$y
+        par$mu1 <- polarToRect(par$angle, par$drift)$x
+        par$mu2 <- polarToRect(par$angle, par$drift)$y
     }
     return(par)
 }
+
