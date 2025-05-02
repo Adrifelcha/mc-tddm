568a5114a3b8bc9219ea4d6042879f6e042f709b
Adriana Chavez <adrifelcha@gmail.com>
2025-05-01 19:47:31 -0700

    Add: A short script to complete a parameter list by adding rect/polar coordinates

---
+++ src/set_up/complete_parList.R
@@ -0,0 +1,11 @@
+complete_par_list <- function(par){
+    if(is.null(par$drift)){
+    par$drift <- rectToPolar(par$mu1, par$mu2)$dLength
+    par$angle <- rectToPolar(par$mu1, par$mu2)$dAngle
+    }
+    if(is.null(par$mu1)){
+        par$mu1 <- polarToRect(par$drift, par$angle)$x
+        par$mu2 <- polarToRect(par$drift, par$angle)$y
+    }
+    return(par)
+}
